80da3c0b987165fa629a4c5c5e47aca6c7ebafe5
Esa Varemo <esa@kuivanto.fi>
2012-05-10 19:17:59 +0300

    A real model for the player-data

--- models.py
+++ models.py
@@ -2,3 +2,10 @@ from google.appengine.ext import db
 
 class String(db.Model):
     data = db.StringProperty(required=True)
+
+class PlayerCount(db.Model):
+    timestamp = db.IntegerProperty(required=true)
+    
+    A3 = db.IntegerProperty(required=true)
+    WC = db.IntegerProperty(required=true)
+    AD = db.IntegerProperty(required=true)
